849135793f47727e2a1ae2023634aa6c2152919b
alexissorianooo <alexis.soriano27@gmail.com>
2025-09-23 15:51:57 +0800

    Adds new asset icon for back button

---
+++ lib/core/assets/assets_enum.dart
@@ -0,0 +1,7 @@
+class AssetsEnum {
+  static const String iconPath = 'lib/core/assets/icons/';
+  static const String imagePath = 'lib/core/assets/images/';
+
+  // Icons
+  static const String backArrow = '${iconPath}back_arrow.svg';
+}
--- lib/features/workout/presentation/screen/workout_add_screen.dart
+++ lib/features/workout/presentation/screen/workout_add_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/assets/assets_enum.dart';
 
 class WorkoutAddScreen extends StatelessWidget {
   const WorkoutAddScreen({super.key});
@@ -6,7 +9,15 @@ class WorkoutAddScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Add Workout')),
+      appBar: AppBar(
+        title: const Text('Add Workout'),
+        leading: IconButton(
+          icon: SvgPicture.asset(AssetsEnum.backArrow),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
       body: const Center(child: Text('Workout Add Screen')),
     );
   }
